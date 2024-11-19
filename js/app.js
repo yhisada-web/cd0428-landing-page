f87d37008a9acb46b16547de06b929018dedd002
Yuya Hisada <yuyahisada@hotmail.com>
2024-11-18 17:40:24 -0900

    built the nav and added smooth scrolling option

--- js/app.js
+++ js/app.js
@@ -40,6 +40,47 @@
 
 // build the nav
 
+// Select the navigation bar
+const navBar = document.querySelector('#navbar__list');
+
+const menuSections = document.querySelectorAll('section');
+
+menuSections.forEach(section => {
+    // Get the 'data-nav' attribute from each section
+    const navText = section.getAttribute('data-nav');
+    
+    // Create new list item and link elements
+    const menuItem = document.createElement('li');
+    const linkElement = document.createElement('a');
+    
+    
+
+    // Add the menu__link class to the link elements
+    linkElement.classList.add('menu__link');
+    
+    // Set the text of the link
+    linkElement.innerHTML = navText;
+
+    // Set href of the link with a '#' to correctly reference the ID
+    const navLink = `#${section.getAttribute('id')}`;
+    linkElement.href = navLink;
+
+    // Add the 'data-*' attribute to the list item
+    menuItem.setAttribute('data-view', navLink);
+    
+    // Append the link to the list item
+    menuItem.appendChild(linkElement);
+    
+    // Append the list item to the navigation bar
+    navBar.appendChild(menuItem);
+
+    // Scroll to section on link click 
+    linkElement.addEventListener('click', (event) => { 
+        event.preventDefault();
+        document.querySelector(navLink).scrollIntoView({ behavior: "smooth" });
+    });
+});
+
 
 // Add class 'active' to section when near top of viewport
 
@@ -56,6 +97,7 @@
 // Build menu 
 
 // Scroll to section on link click
+
 
 // Set sections as active
 
